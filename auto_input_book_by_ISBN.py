9f77764b81450fc9b080bc957ee0e71e0e5d7902
nameless85287 <nameless85287@gmail.com>
2025-07-01 18:36:42 +0800

    Update auto_input_book_by_ISBN.py

--- auto_input_book_by_ISBN.py
+++ auto_input_book_by_ISBN.py
@@ -10,9 +10,8 @@ import book_IO
 def find_ISBN_and_Volume(book_ISBN,datas):
     for loc,data in enumerate(datas):
         if book_ISBN in data.text:
-            print("找到index與data")
-            return loc+4,data.text    #根據資料結構<出版年月>會在ISBN搜尋到的位置往下4個index，直接在此回傳
-    return 29,"None None"   #根據網頁資料結構<出版年月>會在index29的地方
+            return data.text    #直接在此回傳<ISBN> 與<裝訂方式和冊數>
+    return "None None"   #沒有則回傳兩個None字串
 
 
 
@@ -53,32 +52,47 @@ def AIBBI():
         soup = BeautifulSoup(html, 'html.parser')
         # print(soup.text)
         datas = soup.find_all('td')
-
-        # for i,data in enumerate(datas):
-        #     print(i,data.text)
-        #     print("--"*15)
-
-        book_publish_ym_Loc,book_ISBN_and_Volume=find_ISBN_and_Volume(book_ISBN,datas)
+        book_ISBN_and_Volume=find_ISBN_and_Volume(book_ISBN,datas)
         # print(book_publish_ym_Loc,book_ISBN_and_Volume)
-        #
         # 用下面這行指令拆分ISBN跟冊數為LIST內容
         split_ISBN_and_Volume=list(book_ISBN_and_Volume.split())
 
-        #
+
 
         #print(split_ISBN_and_Volume)
-        #
-        if split_ISBN_and_Volume[1]=="None":
-            book_name = datas[1].text
-        else:
-            book_name=datas[1].text+split_ISBN_and_Volume[1]
-        book_writer=datas[3].text
-        book_publisher=datas[5].text
-        book_publish_year_month=datas[book_publish_ym_Loc].text
 
+        #找出書名
+        book_name_td = soup.find("td", {"aria-label": "書名"})
+        true_book_name_td = book_name_td.find_next_sibling('td')
+        book_name = true_book_name_td.text
+
+        #加入書本的裝訂方式與冊數內容
+        if split_ISBN_and_Volume[1]!="None":
+            book_name=book_name+split_ISBN_and_Volume[1]
+        #print(book_name)
+
+        #找出作者名稱
+        book_writer_td = soup.find("td", {"aria-label": "作者"})
+        true_book_writer_td = book_writer_td.find_next_sibling('td')
+        book_writer = true_book_writer_td.text
+        #print(book_writer)
+
+        #找出出版機構
+        book_publisher_td = soup.find("td", {"aria-label": "出版機構"})
+        true_book_publisher_td = book_publisher_td.find_next_sibling('td')
+        book_publisher = true_book_publisher_td.text
+        #print(book_publisher)
+
+        #找出出版年月
+        book_year_month_td = soup.find("td", {"aria-label": "出版年月"})
+        book_publish_year_month= book_year_month_td.text
+        print(book_publish_year_month)
+
+        #印出搜尋到的所有資料供使用者檢查
         book_IO.print_book_info(book_name,book_writer,book_publisher,
                                 book_publish_year_month,book_ISBN)
 
+        #讓使用者在檢查資料後決定是否存取資料
         decide=input("您要輸入的書籍資料符合請輸入Y,否請按其他鍵")
         if(decide=="Y"):
             book_IO.write_data_to_excel(book_name,book_writer,book_publisher,
